b44cd70344fa8d66cf81fae528756b3cfed76513
muhamadagung <muhamadagung70674@gmail.com>
2026-09-23 11:28:10 +0700

    first commit

---
+++ src/app/(auth)/sign-out.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const SignOut = () => {
+  return (
+    <View>
+      <Text>SignOut</Text>
+    </View>
+  )
+}
+
+export default SignOut
---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,23 @@
+import "@/global.css";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+
+      <Link href={"/onboarding"} className="p-4 mt-4 bg-primary rounded-4xl text-white">Go to Onboarding</Link>
+      <Link href={"/(auth)/sign-in"} className="p-4 mt-4 bg-primary rounded-4xl text-white">Go to Sign In</Link>
+      <Link href={"/(auth)/sign-up"} className="p-4 mt-4 bg-primary rounded-4xl text-white">Go to Sign Up</Link>
+
+      <Link href={"/subscriptions/spotify"}>Spotify Subscription</Link>
+      <Link href={{
+        pathname: "/subscriptions/[id]",
+        params: {id: "claude"}
+      }}>Claude Max Subscription</Link>
+    </View>
+  );
+}
